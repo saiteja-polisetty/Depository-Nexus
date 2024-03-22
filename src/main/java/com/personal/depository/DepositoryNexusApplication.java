2217be80eb291df7f014e85e66642443cdca452b
Sai Teja Polisetty <9346960887lok@gmail.com>
2024-03-22 19:30:50 +0530

    changes the package name

--- src/main/java/com/personal/depository/DepositoryNexusApplication.java
+++ src/main/java/com/personal/depository/DepositoryNexusApplication.java
@@ -1,4 +1,4 @@
-package com.example.inventory;
+package com.personal.depository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
